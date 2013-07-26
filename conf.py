eb128c2f10594edba27cb3487267aef7aa389b73
Martin Hasoň <martin.hason@gmail.com>
2013-07-26 15:14:20 +0200

    Enabled extension webuni.sphinx.symfonydocs and added settings to merge external repositories

--- conf.py
+++ conf.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
 
-project = 'Symfony dokumentace'
+extensions = ['webuni.sphinx.symfonydocs']
+
+html_title = project = 'Symfony dokumentace'
 master_doc = 'index'
 language = 'cs'
 
+html_theme = 'symfonydocs_cs'
+
 highlight_language = 'php'
-
-# enable highlighting for PHP code not between ``<?php ... ?>`` by default
-from sphinx.highlighting import lexers
-from pygments.lexers.web import PhpLexer
-
-lexers['php'] = PhpLexer(startinline=True)
-lexers['php-annotations'] = PhpLexer(startinline=True)
 
 # use PHP as the primary domain
 primary_domain = 'php'
 
 # set url for API links
 api_url = 'http://api.symfony.com/master/%s'
+
+# set branch for edit links
+html_context = {
+    'github_branch': 'master',
+    'github_repository': 'webuni/symfony-docs-cs'
+}
+
+github_docs = [
+    dict(group_name = 'Symfony dokumentace', repository ='symfony/symfony-docs', sha ='master', target_path = ''),
+    dict(group_name = 'Symfony CMF dokumentace', repository ='symfony-cmf/symfony-cmf-docs', sha ='master', target_path = 'cmf'),
+    dict(group_name = 'SensioFrameworkExtraBundle dokumentace', repository ='sensiolabs/SensioFrameworkExtraBundle', sha ='master', source_path = 'Resources/doc', target_path = 'bundles/SensioFrameworkExtraBundle'),
+    dict(group_name = 'SensioGeneratorBundle dokumentace', repository ='sensiolabs/SensioGeneratorBundle', sha ='master', source_path = 'Resources/doc', target_path = 'bundles/SensioGeneratorBundle'),
+    dict(group_name = 'DoctrineFixturesBundle dokumentace', repository ='doctrine/DoctrineFixturesBundle', sha ='master', source_path = 'Resources/doc', target_path = 'bundles/DoctrineFixturesBundle'),
+    dict(group_name = 'DoctrineMigrationsBundle dokumentace', repository ='doctrine/DoctrineMigrationsBundle', sha ='master', source_path = 'Resources/doc', target_path = 'bundles/DoctrineMigrationsBundle'),
+    dict(group_name = 'DoctrineMongoDBBundle dokumentace', repository ='doctrine/DoctrineMongoDBBundle', sha ='master', source_path = 'Resources/doc', target_path = 'bundles/DoctrineMongoDBBundle'),
+]
